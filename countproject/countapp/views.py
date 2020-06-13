c3a6860b4dad85fc60baf658bf6a6b9ab92f5d52
jesuiszoe <jesuiszoekim@gmail.com>
2020-06-13 19:45:09 +0900

    new

---
+++ countproject/countapp/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request,'home.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def count(request):
+    entered_text = request.GET['fulltext']
+    word_list = entered_text.split()
+
+    word_dictionary={}
+    
+    for word in word_list:
+        if word in word_dictionary:
+            word_dictionary[word] += 1
+        else:
+            word_dictionary[word] = 1
+
+    return render(request,'count.html', 
+    {'alltext':entered_text, 'total': len(word_list),
+    'dictionary': word_dictionary.items()})
